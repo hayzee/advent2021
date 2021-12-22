39d711ca27471183347c4dcfe6005076a623ac31
hayzee <hayzee@users.noreply.github.com>
2021-12-22 23:30:16 +0000

    Day4 - WIP

--- src/advent2021/day04_02.clj
+++ src/advent2021/day04_02.clj
@@ -1,21 +1,18 @@
 (ns advent2021.day04-02)
 
 (defn read-data
-  ;
   [file]
   (->>
     (clojure.string/split (slurp file) #"\r\n")))
 
-; test - keep for now
-;(def file-data (read-data "resources/day4-input-test.txt"))
-(def file-data (read-data "resources/day4-input.txt"))
+(def file-data (read-data "resources/day4-input-test.txt"))
+;;(def file-data (read-data "resources/day4-input.txt"))
 
 (defn get-bingo-calls
   [file-data]
   (read-string
     (str "[" (first file-data) "]")))
 
-; test
 (def bingo-calls (get-bingo-calls file-data))
 
 (defn get-next-board
@@ -40,74 +37,74 @@
   [file-data]
   (vec (reverse (get-all-boards (drop 1 file-data)))))
 
-; test
 (def board-data (get-board-data file-data))
 
-(defn check-number
-  [number board]
-  (->>
-    (apply concat board)
-    (mapv #(if (= number %) (- number) %))
-    (partition 5)
-    (mapv vec)))
+;(defn check-number
+;  [number board]
+;  (->>
+;    (apply concat board)
+;    (mapv #(if (= number %) (- number) %))
+;    (partition 5)
+;    (mapv vec)))
+;
+;; test
+;; (def board (first (get-board-data file-data)))
 
-; test
-; (def board (first (get-board-data file-data)))
 (defn rows
   [board]
   board)
-
+;
 (defn cols
   [board]
   (for [n (range (count (first board)))]
     (map #(nth % n) board)))
-
+;
 (defn filled? [row-or-col]
   (every? neg? row-or-col))
-
-(defn score-board
-  [board]
-  (reduce + (filter pos? (apply concat (cols board)))))
-
-(defn winning-board?
-  [board]
-  (let [all-rowcols (concat (rows board) (cols board))]
-    (when (some filled? all-rowcols)
-      (score-board board))))
-
-
-(winning-board?
-  [[1 -2 3 -4]
-   [-1 2 -3 -4]
-   [-1 2 3 4]
-   [-1 2 3 -4]])
-
-(score-board
-  [[1 -2 3 -4]
-   [-1 -2 -3 -4]
-   [-1 2 3 -4]
-   [-1 2 3 -4]])
-
-(defn update-boards
-  [boards num]
-  (map (partial check-number num) boards))
-
-(defn apply-bingo-calls
-  [boards bingo-calls]
-  (if (seq bingo-calls)
-    (let [this-bingo-call (first bingo-calls)
-          updated-boards (update-boards boards this-bingo-call)
-          board-score (some winning-board? updated-boards)]
-      (if board-score
-        {:boards    updated-boards
-         :last-call this-bingo-call
-         :score     (* this-bingo-call board-score)}
-        (apply-bingo-calls
-          updated-boards
-          (rest bingo-calls))))
-    nil))
-
-(apply-bingo-calls board-data bingo-calls)
+;
+;(defn score-board
+;  [board]
+;  (reduce + (filter pos? (apply concat (cols board)))))
+;
+;(defn winning-board?
+;  [board]
+;  (let [all-rowcols (concat (rows board) (cols board))]
+;    (when (some filled? all-rowcols)
+;      (score-board board))))
+;
+;
+;(winning-board?
+;  [[1 -2 3 -4]
+;   [-1 2 -3 -4]
+;   [-1 2 3 4]
+;   [-1 2 3 -4]])
+;
+;(score-board
+;  [[1 -2 3 -4]
+;   [-1 -2 -3 -4]
+;   [-1 2 3 -4]
+;   [-1 2 3 -4]])
+;
+;(defn update-boards
+;  [boards num]
+;  (map (partial check-number num) boards))
+;
+;(defn apply-bingo-calls
+;  [boards bingo-calls]
+;  (if (seq bingo-calls)
+;    (let [this-bingo-call (first bingo-calls)
+;          updated-boards (update-boards boards this-bingo-call)
+;          board-score (some winning-board? updated-boards)]
+;      (if board-score
+;        {:boards    updated-boards
+;         :last-call this-bingo-call
+;         :score     (* this-bingo-call board-score)}
+;        (apply-bingo-calls
+;          updated-boards
+;          (rest bingo-calls))))
+;    nil))
+;
+;(apply-bingo-calls board-data bingo-calls)
 
 ;; part2
 
@@ -127,16 +124,24 @@
 (defn is-winning-board?
   [board]
   (let [all-rowcols (concat (rows board) (cols board))]
-    (some filled? all-rowcols)))
+    (keyword (str (boolean (some filled? all-rowcols))))))
 
-(defn check-board [board]
-  {:board board
-   :is-winning-board? (is-winning-board? board)}
-  )
+;(keyword (str false))
+;(defn check-board [board]
+;  {:board board
+;   :is-winning-board? (is-winning-board? board)}
+;  )
 
-(def board-data2 board-data)
+;(def board-data2 (map check-board board-data))
 
-(map #(check-board (update-board % 1)) board-data2)
+(defn make-call [board-data call-number]
+  (map #(update-board % call-number) board-data))
+
+(defn split-winners-losers [board-data]
+  (merge
+    {:true [] :false []}
+    (group-by is-winning-board? board-data)))
+
+(get {:this :that} :that :notfound)
 
 (hash-map 1 2)
-
